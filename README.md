0f6ca53b53934e1e6f7b83fe934091fdfd48923e
Aulia Rahman gnteng <83273573+Rhmn8@users.noreply.github.com>
2021-12-12 19:58:52 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# sp4mmers
-jangan disalah gunakan ya!
+## HAI SAYA RAHMAN GANZ DAN AISYAH CANS
+
+
+## INSTALL FOR TERMUX
+
+```
+
+• pkg install update
+
+• pkg install upgrade
+
+• pkg install python
+
+• pkg install git
+
+• pip install request
+
+• pip install lolcat
+
+• pkg install figlet
+
+• git clone https://github.com/Rhmn9/spammers
+
+• cd spammers
+
+• sh spammers.sh
+
+```
+
+## THANKS TOO
+
+```
+
+~ ALLAH SWT
+
+~ EMAK
+
+~ AULIA RAHMAN
+
+~ RIZKY4YOU
+
+~ MR.P94N9
+
+~ MR TYO
+
+~ MR.18 (IRVAN)
+
+~ MCHACKC++
+
+~ MR. X044 XPLOID
+
+```
+
+## FOLLOW GITHUB | SOSMED ME
+<center>
+
+  <a href="https://bit.ly/AuliaRahmanOfficialYT">YouTube</a>
+<br></br>
+  <a href="https://wa.me/625821676621">Whatsapp Businnes</a>
+<br></br>
+  <a href="https://github.com/Rhmn8">GITHUB ME</a>
+<br></br>
+  <a href="">LOVE YOU, AKU CINTA SAMA KAMU</a>
+
+</center>
+
---
+++ spammers.sh
@@ -0,0 +1,287 @@
+#!/system/xbin/bash
+#author : rahman ganz
+#Youtube : https://bit.ly/AuliaRahmanOfficialYT
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+figlet ONEXPLOIT TEAM | lolcat
+sleep 3
+echo "JANGAN LUPA SUBSCRIBE"
+echo "https://bit.ly/AuliaRahmanOfficialYT"
+echo "Subscribe subscribe subscribe subscribe subscribe subscribe"
+sleep 7
+clear
+echo $blue "Loading 5%.."
+sleep 1
+clear
+echo $green "Loading 15%..."
+sleep 1
+clear
+echo $yellow "Loading 45%...."
+sleep 1
+clear
+echo $purple "Loading 65%....."
+sleep 1
+clear
+echo $blue "Loading 85%......"
+sleep 1
+clear
+echo $yellow "Loading 90%......."
+sleep 1
+clear
+echo $blue "Selesai 100%........"
+sleep 1
+clear
+echo $yellow "I LOVE YOU AKU SAYANG KAMU"
+sleep 4
+clear
+figlet ONEXPLOIT TEAM | lolcat
+echo "=====================================================" | lolcat
+echo "+ AUTHOR : AULIA RAHMAN                             +" | lolcat
+echo "+ CONTACT : 085821676621                            +" | lolcat
+echo "+ TEAM : ONEXPLOIT CYBER KALTENG              +" | lolcat
+echo "+ YOUTUBE : AULIA RAHMAN OFFICIAL                   +" | lolcat
+echo "+ LINK : https://bit.ly/AuliaRahmanOfficialYT       +" | lolcat
+echo "+ SUBSCRIBE + LIKE + KOMEN + AND SHARE              +" | lolcat
+echo "=====================================================" | lolcat
+echo
+echo "[1],SPAM CALL 1"|lolcat
+echo $green "[==========================[>"
+echo "[2],SPAM CALL 2"|lolcat
+echo $cyan "[==========================[>"
+echo "[3],SPAM BRUTAL SMS"|lolcat
+echo $red "[==========================[>"
+echo "[4],SPAM UNLIMITED SMS"|lolcat
+echo $yellow "[==========================[>"
+echo "[5],SPAM SMS/WA"|lolcat
+echo $blue "[==========================[>"
+echo "[6],BACKDOOR"|lolcat
+echo $yellow "[==========================[>"
+echo "[7],TOMBOL TERMUX (terkey)"|lolcat
+echo $red "[==========================[>"
+echo "[8],MX TOOLS"|lolcat
+echo $purple "[==========================[>"
+echo "[9],SCRIPT DEFACE"|lolcat
+echo $blue "[==========================[>"
+echo "[10],ANIMASI KARETA"|lolcat
+echo $green "[==========================[>"
+echo "[11],INSTALL TERMUX-UBUNTU"|lolcat
+echo $cyan "[==========================[>"
+echo "[12],INSTALL VTOOL"|lolcat
+echo $red "[==========================[>"
+echo "[13],INSTALL MINITOOLS"|lolcat
+echo $yellow "[==========================[>"
+echo "[14],RED HAWK"|lolcat
+echo $blue "[==========================[>"
+echo "[15],SANTET ONLINE"|lolcat
+echo $purple "[==========================[>"
+echo "[16],INSTALL GCOSPAM"|lolcat
+echo $cyan "[==========================[>"
+echo "[17],INSTALL WIFI-HACKER"|lolcat
+echo $red "[==========================[>"
+echo "[18],INSTALL ANDROID-MALWARE"|lolcat
+echo $yellow "[==========================[>"
+echo "[19],INSTALL SQLMAP"|lolcat
+echo $blue "[==========================[>"
+echo "[20],INSTALL PRANK"|lolcat
+echo $purple "[==========================[>"
+echo "[21],INSTALL GITHUB BOT"|lolcat
+echo $blue "[=============================[>"
+echo "[22],SPAM CALL"|lolcat
+echo $red "[=============================[>"
+echo "[00],keluar"|lolcat
+echo $cyan "[==========================[>"
+echo
+read -p "root@oneXploit~#" pil :
+
+#untuk script nya ya kak
+if [ $pil = 1 ]
+then
+git clone https://github.com/termuxandi/spamtelphone
+cd spamtelphone
+ls
+python Blackphone
+fi
+#untuk script nya ya kak!
+if [ $pil = 2 ]
+then
+git clone https://github.com/termuxandi/spam-termux
+cd spam-termux
+ls
+python SpamC.py
+fi
+#untuk script nya ya kak!
+if [ $pil = 3 ]
+then
+git clone https://github.com/IL4NGQW3R/brutalspammer
+cd brutalspammers
+ls
+python BrutalSpammer.py
+fi
+#untuk scriptnya ya kak!
+if [ $pil = 4 ]
+then
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+ls
+sh LITESPAM.sh
+fi
+#untuk scriptnya ya kak!
+if [ $pil = 5 ]
+then
+git clone https://github.com/KANG-NEWBIE/SpamSms
+cd SpamSms
+ls
+python main.py
+fi
+#untuk scriptnya ya kak!
+if [ $pil = 6 ]
+then
+git clone https://github.com/3xploit-db/backdoor
+ls
+cd backdoor
+fi
+#update script lgi ya kak!
+if [ $pil = 7 ]
+then
+git clone https://github.com/karjok/terkey
+ls
+cd terkey
+python terkey.py
+fi
+#update script lgi ya kak!
+if [ $pil = 8 ]
+then
+git clone https://github.com/rootM3eX/Mx-Tools
+ls
+cd Mx-Tools
+sh Mx-Tool.sh
+fi
+#update script lgi ya kak!
+if [ $pil = 9 ]
+then
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+#script animasi kareta
+if [ $pil = 10 ]
+then
+clear
+pkg install sl
+sl
+fi
+#script ubuntu ya kak!
+if [ $pil = 11 ]
+then
+git clone https://github.com/Neo-Oli/termux-ubuntu
+cd termux-ubuntu
+sh ubuntu.sh
+fi
+#script VTools ya kak!
+if [ $pil = 12 ]
+then
+git clone https://github.com/rootM3eX/VTools
+cd VTools
+fi
+#jangan salah gunakan yaaa kak!
+
+#script MiniTools Ya kak Tapi Jangan Salah Gunakan yaa
+if [ $pil = 13 ]
+then
+git clone https://github.com/rootM3eX/MiniTools
+cd MiniTools
+sh aaa.sh
+fi
+#script RedHawk Ya kak!
+if [ $pil = 14 ]
+then
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+#mau santet online kah mbak? silahkan nih sc nya tapi jangan salah gunakan ya
+if [ $pil = 15 ]
+then
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+fi
+#script gcspam ya kak tapi jangan salah gunakan
+if [ $pil = 16 ]
+then
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+fi
+#update script wifi-hacker ya kak tapi jangan salah gunakan
+if [ $pil = 17 ]
+then
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+./wifi-hacker.sh
+fi
+#update script android-malware tapi jangan salh gunakan
+if [ $pil = 18 ]
+then
+git clone https://github.com/ashishb/android-malware
+cd android-malware
+fi
+#update script sqlmap tapi jangan salah gunakan yaa kak!
+if [ $pil = 19 ]
+then
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+fi
+#update script prank tapi ini hanya hiburan saja ya jangan salah gunakan
+if [ $pil = 20 ]
+then
+git clone https://github.com/siputra12/prank
+cd prank
+php call.php
+fi
+#update github wa bot mastah rahman
+if [ $pil = 21 ]
+then
+git clone https://github.com/Rhmn8/botauliav1
+cd botauliav1
+npm install
+npm start
+fi
+#punya by indonesia cyber xploid
+if [ $pil = 22 ]
+then
+git clone https://github.com/IndonesiaCyberXploid/spam-call
+cd spam-call
+python2 spam-call.py
+fi
+#terakhir buat keluar dari github
+if [ $pil = 00 ]
+then
+
+sleep 3
+clear
+echo $green "Loading.."
+sleep 1
+clear
+echo $yellow "Loading..."
+sleep 2
+clear
+
+sleep 1
+echo $green "Thanks you | ONEXPLOIT CYBER KALTENG | MR. TERSAKITI | INDONESIA CYBER XPLOID | CSA TEAM | CYBER TEAM INDONESIA | UNEXPECTEDSKILL"
+sleep 1
+echo $yellow "see you"
+sleep 1
+echo $red "bye..."
+sleep 1
+fi
